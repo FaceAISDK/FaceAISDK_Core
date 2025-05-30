4c9786210a52df1b39b0a5233027edf18a144cb2
FaceSDK <FaceAISDK.Service@gmail.com>
2025-05-30 15:34:53 +0800

    CI CD 流程测试验证

---
+++ FaceAISDK_Core/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FaceAISDK_Core
+//
+//  Created by anylife on 2025/5/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FaceAISDK_Core/FaceAISDK_CoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceAISDK_CoreApp.swift
+//  FaceAISDK_Core
+//
+//  Created by anylife on 2025/5/30.
+//
+
+import SwiftUI
+
+@main
+struct FaceAISDK_CoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
